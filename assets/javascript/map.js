dcc00b6b3fc60f41d22b496e76a846737a257545
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-01-24 15:53:32 -0600

    hope this works

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,88 +1,72 @@
-function initMap() {
-  
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 38.8339,
-      lng: -104.8214
-    },
-    zoom: 13
-  });
+
  
-    
-  var card = document.getElementById('pac-card');
-  var input = document.getElementById('pac-input');
-  console.log(input);
-  // var types = document.getElementById('type-selector');
-  // possibly remove this var strictBounds
-  // var strictBounds = document.getElementById('strict-bounds-selector');
+  function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 38.8339,
+        lng: -104.821},
+      zoom: 13,
+      mapTypeId: 'roadmap'
+    });
 
-  // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
 
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    $("#add-travel", ).on("click", function () {
+      var places = searchBox.getPlaces();
 
-  // Bind the map's bounds (viewport) property to the autocomplete object,
-  // so that the autocomplete requests use the current map bounds for the
-  // bounds option in the request.
-  autocomplete.bindTo('bounds', map);
-
-  // Set the data fields to return when the user selects a place.
-  autocomplete.setFields(
-    ['address_components', 'geometry', 'name']);
-
-  var infowindow = new google.maps.InfoWindow();
-  var infowindowContent = document.getElementById('infowindow-content');
-  infowindow.setContent(infowindowContent);
-  var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
-
-  autocomplete.addListener('place_changed', function () {
-    infowindow.close();
-    marker.setVisible(false);
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
-    }
-
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17); // Why 17? Because it looks good.
-    }
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-
-    // var address = '';
-    // if (place.address_components) {
-    //   address = [
-    //     (place.address_components[0] && place.address_components[0].short_name || ''),
-    //     (place.address_components[1] && place.address_components[1].short_name || ''),
-    //     (place.address_components[2] && place.address_components[2].short_name || '')
-    //   ].join(' ');
-    // }
-
-    // infowindowContent.children['place-icon'].src = place.icon;
-    infowindowContent.children['place-name'].textContent = place.name;
-    // infowindowContent.children['place-address'].textContent = address;
-    infowindow.open(map, marker);
-  });
-
-      autocomplete.setOptions({
-        strictBounds: this.checked
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+      marker.setMap(null);
       });
-}
+      markers = [];
 
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  };
+
+  
 //-this is Giffy area-//
-$(function () {
-
-  $("button", ).on("click", function () {
+  $("button").on("click", function () {
       var type = $(this).attr("travel-place");
       $("#imageArea").empty();
 
@@ -93,7 +77,9 @@ $(function () {
               method: 'GET'
 
           })
+
           .then(function (response) {
+
               console.log(response);
               var results = response.data;
               for (var i = 0; i < response.data.length; i++) {
@@ -107,15 +93,17 @@ $(function () {
                   gify.prepend(p);
                   $("#imageArea").prepend(gify);
               }
+
           });
   });
-});
+
+
 $("#add-travel").on("click", function (event) {
   var search = $("#travel-input").val().trim();
   console.log(search);
   event.preventDefault();
   var a = $("<button>").html(search);
-  a.addClass("btn btn-primary");
+  a.addClass("btn btn-secondary");
   a.val(search);
   $("#buttonArea").append(a);
   console.log(a);
@@ -149,4 +137,4 @@ $("#add-travel").on("click", function (event) {
           });
 
   });
-});
+})
